fbbc3201ca4d62c07afdbdc377ea88dfe781a5cf
parksungchan <posco123!>
2017-02-28 17:54:01 +0900

    wf net config set get

--- demo/api_cnn_img_conf.py
+++ demo/api_cnn_img_conf.py
@@ -4,14 +4,69 @@ from common.utils import *
 
 println("S")
 
-resp = requests.post('http://' + gUrl + '/api/v1/type/wf/state/netconf/detail/cnn/nnid/nn00004/ver/5/node/5/',
+nn_id = "nn00004"
+wf_ver_id = "5"
+node_id = "5"
+
+resp = requests.put('http://' + gUrl + '/api/v1/type/wf/state/netconf/detail/cnn/nnid/'+nn_id+'/ver/'+wf_ver_id+'/node/'+node_id+'/',
                      json={
-                         "nn_id": "nn00004",
-                         "wf_id": "5",
-                         "node_id": "5"
+                         "key" : {"nn_id": nn_id,
+                                 "wf_ver_id": wf_ver_id,
+                                 "node_id": node_id
+                                  }
+                         ,"config": {"matrix": [4, 5],
+                                 "learnrate": 0.01,
+                                 "epoch": 2,
+                                 "x_shape":[0,0],
+                                 "y_shape":[0,0]
+                                 }
+                         ,"layer": {
+                                 "type": "cnn",
+                                 "active": "relu",
+                                 "cnnfilter": [2, 2],
+                                 "cnnstride": [1, 1],
+                                 "maxpoolmatrix": [2, 2],
+                                 "maxpoolstride": [1, 1],
+                                 "node_in_out": [1, 32],
+                                 "regualizer": "",
+                                 "padding": "SAME",
+                                 "droprate": ""
+                                }
+                         ,"drop": {
+                                 "active": "tanh",
+                                 "cnnfilter": [2, 2],
+                                 "cnnstride": [1, 1],
+                                 "maxpoolmatrix": [2, 2],
+                                 "maxpoolstride": [1, 1],
+                                 "node_in_out": [32, 64],
+                                 "regualizer": "",
+                                 "padding": "SAME",
+                                 "droprate": "0.1"
+                                }
+                          ,"out": {
+                                 "active": "softmax",
+                                 "cnnfilter": "",
+                                 "cnnstride": "",
+                                 "maxpoolmatrix": "",
+                                 "maxpoolstride": "",
+                                 "node_in_out": "",
+                                 "regualizer": "",
+                                 "padding": "SAME",
+                                 "droprate": ""
+                                }
+                        })
+data = json.loads(resp.json())
+print("evaluation result : {0}".format(data))
+
+# search nn_info
+resp = requests.get('http://' + gUrl + '/api/v1/type/wf/state/netconf/detail/cnn/nnid/'+nn_id+'/ver/'+wf_ver_id+'/node/'+node_id+'/',
+                    json={
+                        "key": {"nn_id": nn_id,
+                                "wf_ver_id": wf_ver_id,
+                                "node_id": node_id
+                                }
                      })
 data = json.loads(resp.json())
-# print("evaluation result : {0}".format(data))
-
+print("evaluation result : {0}".format(data))
 
 println("E")
--- master/workflow/netconf/workflow_netconf_cnn.py
+++ master/workflow/netconf/workflow_netconf_cnn.py
@@ -1,22 +1,52 @@
 from master.workflow.netconf.workflow_netconf import WorkFlowNetConf
 from common.utils import *
+from master import models
+from django.core import serializers as serial
+import json
 
 class WorkFlowNetConfCNN(WorkFlowNetConf):
     """
 
     """
-    def get_view_obj(self):
+    def get_view_obj(self, input_data):
         """
         get view data for net config
         :return:
         """
+        nn_id = input_data["key"]["nn_id"]
+        wf_ver_id = input_data["key"]["wf_ver_id"]
+        node_id = input_data["key"]["node_id"]
+        state_id = nn_id + "_" + wf_ver_id
+
+        println("WorkFlowNetConfCNN get_view_obj nn_id="+nn_id+" wf_ver_id="+wf_ver_id+" node_id="+node_id+" state_id="+state_id)
+
+        try:
+            obj = models.NN_WF_NODE_INFO.objects.get(wf_state_id_id=state_id, nn_wf_node_id=node_id)
+            data_set = getattr(obj, "node_config_data")
+            return data_set
+        except Exception as e:
+            raise Exception(e)
         return None
 
-    def set_view_obj(self, obj):
+    def set_view_obj(self, input_data):
         """
         set net config data edited on view
         :param obj:
         :return:
         """
-        println("workflownetconfcnn.set_view_obj.....")
+        nn_id = input_data["key"]["nn_id"]
+        wf_ver_id = input_data["key"]["wf_ver_id"]
+        node_id = input_data["key"]["node_id"]
+        state_id = nn_id+"_"+wf_ver_id
+
+        println("WorkFlowNetConfCNN set_view_obj nn_id="+nn_id+" wf_ver_id="+wf_ver_id+" node_id="+node_id+" state_id="+state_id)
+
+        try:
+            obj = models.NN_WF_NODE_INFO.objects.get(wf_state_id_id=state_id, nn_wf_node_id=node_id)
+            setattr(obj, "node_config_data", input_data)
+            obj.save()
+            return input_data
+        except Exception as e:
+            raise Exception(e)
         return None
+
