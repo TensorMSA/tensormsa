67fa5c799549ec7edbbf4337d78c956f450783d9
healess <healess1@gmail.com>
2017-03-01 13:58:59 +0000

    Built in automated testing F/W to free from errors

---
+++ runtests.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+This is the test runner for the HOYA AI's Django tests.
+"""
+
+import os
+import sys
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+if __name__ == '__main__':
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'hoyai.settings'
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(['tests'])
+sys.exit(bool(failures))
---
+++ tests/unitCase.py
@@ -0,0 +1,12 @@
+class TestMethod():
+    def returnString(param):
+    	return 'successful'
+
+    def returnUpper(self, param):
+    	return param.upper()
+
+    def checkUpper(self, param):
+    	return param.isupper()
+
+    def splitString(self, param):
+    	return param.split()
