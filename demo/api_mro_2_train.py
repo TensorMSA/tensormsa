aed04bf470cb6cfe1c4397e05be01c2bf6dd7b3b
parksungchan <soul8085@gmail.com>
2017-03-02 17:01:54 +0900

    mro network create api sample

---
+++ demo/api_mro_2_train.py
@@ -0,0 +1,36 @@
+import requests
+import json, os
+from common.utils import *
+
+# [TEST - Celery]
+# apt-get install rabbitmq-server
+# service rabbitmq-server start
+# rabbitmqctl add_user tensormsa tensormsa
+# rabbitmqctl set_user_tags tensormsa administrator
+# celery -A hoyai worker -l info
+# ./manage.py runserver [HOST]:8000
+
+println("S")
+
+nn_id = "nn00004"
+
+# get workflow version info
+resp = requests.get('http://' + gUrl + '/api/v1/type/common/target/nninfo/'+nn_id+'/version/')
+data = json.loads(resp.json())
+
+wf_ver_id = 0
+for i in data:
+    if i["pk"] > wf_ver_id:
+        wf_ver_id = i["pk"]
+
+wf_ver_id = str(wf_ver_id)
+
+# Run All Workflow
+resp = requests.post('http://' + gUrl + '/api/v1/type/runmanager/state/train/nnid/'+nn_id+'/ver/'+wf_ver_id+'/')
+data = json.loads(resp.json())
+print("evaluation result : {0}".format(data))
+
+println("E")
+
+
+
