5eea8082188a67ac842e7fcea8599f7b723cb200
healess <healess1@gmail.com>
2017-03-03 19:03:19 +0900

    Add Sample code for tests

---
+++ tests/test_api_basic_1_net_info.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+class test_api_basic_1_net_info(TestCase):
+        def test_callAPI(self):
+            from demo import api_basic_1_net_info
+            print("API Test")
