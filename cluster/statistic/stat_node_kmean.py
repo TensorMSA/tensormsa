dd8edf25eabec12875b07e9a4bbf9e460f03e94f
healess <healess1@gmail.com>
2017-03-07 23:22:44 +0900

    Initialize statistic Nodes.

---
+++ cluster/statistic/stat_node_kmean.py
@@ -0,0 +1,15 @@
+from cluster.statistic.stat_node import StatNode
+
+class StaeNodeKmean(StatNode):
+    """
+
+    """
+
+    def run(self, conf_data):
+        return None
+
+    def _init_node_parm(self):
+        return None
+
+    def _set_progress_state(self):
+        return None
