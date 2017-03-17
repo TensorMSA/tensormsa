d37644cafdce7d8c41968a07685a2383d46a7cb1
healess <healess1@gmail.com>
2017-03-17 23:10:53 +0900

    Add Wf_Init for doc_to_vec

---
+++ demo/api_basic_4_data_d2v.py
@@ -0,0 +1,40 @@
+import requests
+import json, os
+
+url = "{0}:{1}".format(os.environ['HOSTNAME'] , "8000")
+
+nn_id = "nn00006"
+wf_ver_id = "107"
+
+# update source_info
+resp = requests.post('http://' + url + '/api/v1/type/wf/state/textdata/src/local/form/raw/prg/source/nnid/' + nn_id + '/ver/' + wf_ver_id + '/node/data_node/',
+                     json={
+                         "source_server": "local",
+                         "source_sql": "all",
+                         "source_path": "test",
+                         "max_sentence_len" : 50
+                     })
+data = json.loads(resp.json())
+print("evaluation result : {0}".format(data))
+
+# update preprocess
+# preprocess : kkma, twiter, mecab, nltk
+resp = requests.post('http://' + url + '/api/v1/type/wf/state/textdata/src/local/form/raw/prg/pre/nnid/' + nn_id + '/ver/' + wf_ver_id + '/node/data_node/',
+                     json={
+                         "preprocess":  "mecab",
+                     })
+data = json.loads(resp.json())
+print("evaluation result : {0}".format(data))
+
+# update store_path
+resp = requests.post('http://' + url + '/api/v1/type/wf/state/textdata/src/local/form/raw/prg/store/nnid/' + nn_id + '/ver/' + wf_ver_id + '/node/data_node/',
+                     json={
+                         "store_path": "d2v"
+                     })
+data = json.loads(resp.json())
+print("evaluation result : {0}".format(data))
+
+# check properties are updated well
+resp = requests.get('http://' + url + '/api/v1/type/wf/state/textdata/src/local/form/raw/prg/source/nnid/' + nn_id + '/ver/' + wf_ver_id + '/node/data_node/')
+data = json.loads(resp.json())
+print("evaluation result : {0}".format(data))
