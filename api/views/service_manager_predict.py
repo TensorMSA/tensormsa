a9fe8336313150736ea0a603227a37064e8a8c88
seungwookim <tmddno1@naver.com>
2017-03-21 12:54:30 +0900

    modify predict api to accept both ver specified and active version auto find (To-Do : need to implement version selected case)

--- api/views/service_manager_predict.py
+++ api/views/service_manager_predict.py
@@ -14,23 +14,36 @@ class ServiceManagerPredict(APIView):
         - desc : insert cnn configuration data
         """
         try:
-            if(type == 'w2v') :
-                input_data = json.loads(str(request.body, 'utf-8'))
-                return_data = PredictNetW2V().run(nnid, input_data)
-            elif(type == "d2v"):
-                input_data = json.loads(str(request.body, 'utf-8'))
-                return_data = PredictNetD2V().run(nnid, input_data)
-            elif(type == "cnn"):
-                return_data = PredictNetCnn().run(nnid, ver, request.FILES)
+            if(ver == 'active') :
+                if(type == 'w2v') :
+                    input_data = json.loads(str(request.body, 'utf-8'))
+                    return_data = PredictNetW2V().run(nnid, input_data)
+                elif(type == "d2v"):
+                    input_data = json.loads(str(request.body, 'utf-8'))
+                    return_data = PredictNetD2V().run(nnid, input_data)
+                elif(type == "cnn"):
+                    # TO-DO : need predict function for active taged version
+                    raise Exception("on developing now !")
+                else :
+                    raise Exception ("Not defined type error")
             else :
-                raise Exception ("Not defined type error")
+                if (type == 'w2v'):
+                    # TO-DO : need predict function for specific  version
+                    raise Exception("on developing now !")
+                elif (type == "d2v"):
+                    # TO-DO : need predict function for specific  version
+                    raise Exception("on developing now !")
+                elif (type == "cnn"):
+                    return_data = PredictNetCnn().run(nnid, ver, request.FILES)
+                else:
+                    raise Exception("Not defined type error")
 
             return Response(json.dumps(return_data))
         except Exception as e:
             return_data = {"status": "404", "result": str(e)}
             return Response(json.dumps(return_data))
 
-    def get(self, request, type, nnid):
+    def get(self, request, type, nnid, ver):
         """
         - desc : get cnn configuration data
         """
@@ -41,7 +54,7 @@ class ServiceManagerPredict(APIView):
             return_data = {"status": "404", "result": str(e)}
             return Response(json.dumps(return_data))
 
-    def put(self, request, type, nnid):
+    def put(self, request, type, nnid, ver):
         """
         - desc ; update cnn configuration data
         """
@@ -52,7 +65,7 @@ class ServiceManagerPredict(APIView):
             return_data = {"status": "404", "result": str(e)}
             return Response(json.dumps(return_data))
 
-    def delete(self, request, type, nnid):
+    def delete(self, request, type, nnid, ver):
         """
         - desc : delete cnn configuration data
         """
