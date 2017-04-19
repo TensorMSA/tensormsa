247b1cd3c8fdac498095c1e34eae48334b32e0bf
parksungchan <soul8085@gmail.com>
2017-04-19 11:29:57 +0900

    Update : CNN Train : API Get Workflow Version Logic Update

--- demo/api_mro_3_predict.py
+++ demo/api_mro_3_predict.py
@@ -14,29 +14,39 @@ println("S")
 # ./manage.py runserver [HOST]:8000
 url = gUrl
 typeStr = "cnn"
-nn_id = "nn00004"
-
+nn_id = "nn00009"
+wf_ver_id = 5
 # get workflow version info
-resp = requests.get('http://' + url + '/api/v1/type/common/target/nninfo/nnid/'+nn_id+'/version/')
-data = json.loads(resp.json())
+if wf_ver_id == 0:
+    resp = requests.get('http://' + url + '/api/v1/type/common/target/nninfo/nnid/'+nn_id+'/version/')
+    data = json.loads(resp.json())
 
-for config in data:
-    if config["fields"]["active_flag"] == "Y":
-        wf_ver_id = config["pk"]
+    for config in data:
+        if config["fields"]["active_flag"] == "Y":
+            wf_ver_id = config["pk"]
 
 wf_ver_id = str(wf_ver_id)
 
 files = {
-         'files000001':  open('/home/dev/hoyai/demo/data/airplane/1air.jpg','rb')
-        ,'files000002':  open('/home/dev/hoyai/demo/data/airplane/2air.jpg','rb')
-        ,'files000003':  open('/home/dev/hoyai/demo/data/bolt/1bolt.jpg','rb')
-        ,'files000004':  open('/home/dev/hoyai/demo/data/bolt/2bolt.jpg','rb')
-        ,'files000005':  open('/home/dev/hoyai/demo/data/car/1car.jpg','rb')
-        ,'files000006':  open('/home/dev/hoyai/demo/data/car/2car.jpg','rb')
-        ,'files000007':  open('/home/dev/hoyai/demo/data/glove/1glove.jpg','rb')
-        ,'files000008':  open('/home/dev/hoyai/demo/data/glove/2glove.jpg','rb')
-        ,'files000009':  open('/home/dev/hoyai/demo/data/motor/1motor.jpg','rb')
-        ,'files000010':  open('/home/dev/hoyai/demo/data/motor/2motor.jpg','rb')
+        #  'files000001':  open('/home/dev/hoyai/demo/data/airplane/1air.jpg','rb')
+        # ,'files000002':  open('/home/dev/hoyai/demo/data/airplane/2air.jpg','rb')
+        # ,'files000003':  open('/home/dev/hoyai/demo/data/bolt/1bolt.jpg','rb')
+        # ,'files000004':  open('/home/dev/hoyai/demo/data/bolt/2bolt.jpg','rb')
+        # ,'files000005':  open('/home/dev/hoyai/demo/data/car/1car.jpg','rb')
+        # ,'files000006':  open('/home/dev/hoyai/demo/data/car/2car.jpg','rb')
+        # ,'files000007':  open('/home/dev/hoyai/demo/data/glove/1glove.jpg','rb')
+        # ,'files000008':  open('/home/dev/hoyai/demo/data/glove/2glove.jpg','rb')
+        # ,'files000009':  open('/home/dev/hoyai/demo/data/motor/1motor.jpg','rb')
+        # ,'files000010':  open('/home/dev/hoyai/demo/data/motor/2motor.jpg','rb')
+
+ 'files000001':  open('/hoya_src_root/nn00004/21/personData/PSC/20170418_094849.jpg','rb')
+    ,'files000002':  open('/hoya_src_root/nn00004/21/personData/PJH/20170418_094144.jpg','rb')
+# ,'files000003':  open('/hoya_src_root/nn00004/21/personData/LSH/20170418_094624.jpg','rb')
+
+ #  'files000004':  open('/hoya_src_root/nn00004/21/personDataTest/PSC/20170417_180614.jpg','rb')
+ # ,'files000005':  open('/hoya_src_root/nn00004/21/personDataTest/PJH/20170417_180225(0).jpg','rb')
+# , 'files000006':  open('/hoya_src_root/nn00004/21/personDataTest/LSH/20170417_180436.jpg','rb')
+
         }
 
 restURL = 'http://' + url + '/api/v1/type/service/state/predict/type/'+typeStr+'/nnid/'+nn_id+'/ver/'+wf_ver_id+'/'
