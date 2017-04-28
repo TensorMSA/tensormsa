c13a4e15264449f7e9fa757ec2d54f9ab9fa4ddc
tmddno1 <tmddno1@naver.com>
2017-04-28 18:28:18 +0900

    bugfix word2vec predict get vector error

--- demo/api_basic_8_eval_conf.py
+++ demo/api_basic_8_eval_conf.py
@@ -3,12 +3,12 @@ import json, os
 
 url = "{0}:{1}".format(os.environ['HOSTNAME'] , "8000")
 
-nn_id = 'nn00007'
+nn_id = 'nn00009'
 nn_ver = '1'
 node_name = 'eval_node'
 resp = requests.put('http://' + url + '/api/v1/type/wf/state/eval/nnid/'+nn_id+'/ver/'+nn_ver+'/node/'+node_name+'/',
                     json={
-                        "type": "seq2seq",
+                        "type": "w2v",
                     })
 
 data = json.loads(resp.json())
