e93e52281b0791ec207234a3765ca8c3bb4865f1
Paik Jee Hyun <intwis100@naver.com>
2017-05-10 17:51:31 +0900

    BUGFIX : datetime json

--- api/views/common_nninfo_list.py
+++ api/views/common_nninfo_list.py
@@ -1,4 +1,4 @@
-import json
+from django.core.serializers.json import json, DjangoJSONEncoder
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from master.network.nn_common_manager import NNCommonManager
@@ -31,10 +31,11 @@ class CommonNNInfoList(APIView):
             if str(nnid).lower() == 'all':
                 condition['nn_id'] = '%'
             return_data = NNCommonManager().get_nn_info(condition)
+            logging.info(return_data)
             return Response(json.dumps(return_data))
         except Exception as e:
             return_data = {"status": "404", "result": str(e)}
-            return Response(json.dumps(return_data))
+            return Response(json.dumps(return_data, cls=DjangoJSONEncoder))
 
     def put(self, request, nnid):
         """
