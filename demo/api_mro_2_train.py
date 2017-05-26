34c491ec4fbb4d09f9360d8b5a8759bfc9e54687
parksungchan <soul8085@gmail.com>
2017-05-26 13:49:06 +0900

    Update : CNN Eval : Eval Print Update

--- demo/api_mro_2_train.py
+++ demo/api_mro_2_train.py
@@ -14,7 +14,7 @@ from common.utils import *
 println("S")
 url = "{0}:{1}".format(os.environ['HOSTNAME'] , "8000")
 nn_id = "mro001"
-net_type = "cnn" # cnn, resnet
+net_type = "resnet" # cnn, resnet
 wf_ver_id = 0
 
 # get workflow version
@@ -35,10 +35,11 @@ node = "netconf_node"
 if net_type == "cnn":
     resp = requests.put('http://' + url + '/api/v1/type/wf/state/netconf/detail/cnn/nnid/'+nn_id+'/ver/'+wf_ver_id+'/node/'+node+'/',
                      json={
-                         "param":{"traincnt": 1
-                                  ,"epoch": 2
+                         "param":{"traincnt": 0
+                                  ,"epoch": 1
                                   ,"batch_size":1000000
-                                  ,"predictcnt": 3
+                                  ,"predictcnt": 5
+                                  ,"predlog":"T" # T:Ture, F:False, A:True&False, N:None
                          },
                          "config": {"num_classes":10,
                                     "learnrate": 0.001,
@@ -74,12 +75,13 @@ if net_type == "cnn":
 elif net_type == "resnet":
     resp = requests.put('http://' + url + '/api/v1/type/wf/state/netconf/detail/cnn/nnid/'+nn_id+'/ver/'+wf_ver_id+'/node/'+node+'/',
                      json={
-                         "param":{"traincnt": 3
-                                  ,"epoch": 2
-                                  ,"batch_size":25000
-                                  ,"predictcnt": 10
+                         "param":{"traincnt": 2
+                                  ,"epoch": 10
+                                  ,"batch_size":200
+                                  ,"predictcnt": 5
+                                  , "predlog": "A"  # T:Ture, F:False, A:True&False, N:None
                          },
-                         "config": {"num_classes":10,
+                         "config": {"num_classes":1,
                                     "learnrate": 0.001,
                                     "layeroutputs":18, #18, 34, 50, 101, 152, 200
                                     "net_type":"resnet",
