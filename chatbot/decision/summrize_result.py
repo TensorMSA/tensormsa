c56aab11d90a08a8b5a4dc1aec2593d473c1a4c1
seungwookim <tmddno1@naver.com>
2017-07-10 14:27:00 +0900

    bugfix for del unexist key

--- chatbot/decision/summrize_result.py
+++ chatbot/decision/summrize_result.py
@@ -85,57 +85,62 @@ class SummrizeResult():
         
         :return: 
         """
-        score = 0
-        share_data.set_intent_id(intent_id)
+        try :
+            score = 0
+            share_data.set_intent_id(intent_id)
+            ner_obj = self.ner_obj.copy()
+            dict_obj = self.ner_obj.copy()
 
-        # case0 : if there is no intent essential parms
-        if(len(list(set(essence))) == 0):
-            logging.info("Case0 : cannot understand intent")
-            #share_data.set_intent_id("-1")
-            score = -1
+            # case0 : if there is no intent essential parms
+            if(len(list(set(essence))) == 0):
+                logging.info("Case0 : cannot understand intent")
+                #share_data.set_intent_id("-1")
+                score = -1
 
-        # case1 : best case, predicted intent and common ner anal result sync well
-        if(len(list(set(essence) - set(self.common_keys))) == 0):
-            # trim entities fit to intent slot
-            logging.info("Case1 : perfect case all matches!")
-            del_keys = set(self.common_keys) - set(essence) - set(extra)
-            for key in list(del_keys):
-                del self.ner_obj[key]
-            share_data.replace_story_slot_entity(self.ner_obj)
-            score = 10 + len(essence)
+            # case1 : best case, predicted intent and common ner anal result sync well
+            if(len(list(set(essence) - set(self.common_keys))) == 0):
+                # trim entities fit to intent slot
+                logging.info("Case1 : perfect case all matches!")
+                del_keys = set(self.common_keys) - set(essence) - set(extra)
+                for key in list(del_keys):
+                    del ner_obj[key]
+                share_data.replace_story_slot_entity(ner_obj)
+                score = 10 + len(essence)
 
-        # case2 : intent and dict result matches
-        elif(len(list(set(essence) - set(self.dict_keys))) == 0):
-            # trim entities fit to intent slot
-            logging.info("Case2 : intent and dict result matches")
-            del_keys = set(self.dict_keys) - set(essence) - set(extra)
-            for key in list(del_keys):
-                del self.dict_obj[key]
-            share_data.replace_story_slot_entity(self.dict_obj)
-            score = 7 + len(essence)
+            # case2 : intent and dict result matches
+            elif(len(list(set(essence) - set(dict_obj))) == 0):
+                # trim entities fit to intent slot
+                logging.info("Case2 : intent and dict result matches")
+                del_keys = set(dict_obj) - set(essence) - set(extra)
+                for key in list(del_keys):
+                    del dict_obj[key]
+                share_data.replace_story_slot_entity(dict_obj)
+                score = 7 + len(essence)
 
-        # case3 : predicted intent and bilstm anal matches
-        elif(len(list(set(essence) - set(self.ner_keys))) == 0):
-            # trim entities fit to intent slot
-            logging.info("Case3 : intent and ner result matches")
-            del_keys = set(self.common_keys) - set(essence) - set(extra)
-            for key in list(del_keys):
-                del self.ner_obj[key]
-            share_data.replace_story_slot_entity(self.ner_obj)
-            score = 8 + len(essence)
+            # case3 : predicted intent and bilstm anal matches
+            elif(len(list(set(essence) - set(self.ner_keys))) == 0):
+                # trim entities fit to intent slot
+                logging.info("Case3 : intent and ner result matches")
+                del_keys = set(self.common_keys) - set(essence) - set(extra)
+                for key in list(del_keys):
+                    del ner_obj[key]
+                share_data.replace_story_slot_entity(ner_obj)
+                score = 8 + len(essence)
 
-        # case4 : predicted intent and ner result do not match but common ner exists
-        elif(len(self.common_keys) > 0):
-            # get multiple intent which matches with ner result
-            logging.info("Case4 : intent do not match but ner matches")
-            c_intent_id = self.get_intent_candidate(self.common_keys)
-            share_data.set_intent_id(c_intent_id)
-            score = 5
+            # case4 : predicted intent and ner result do not match but common ner exists
+            elif(len(self.common_keys) > 0):
+                # get multiple intent which matches with ner result
+                logging.info("Case4 : intent do not match but ner matches")
+                c_intent_id = self.get_intent_candidate(self.common_keys)
+                share_data.set_intent_id(c_intent_id)
+                score = 5
 
-        # case5 : error
-        else:
-            logging.info("Case5 : cannot understand input at all")
-            #share_data.set_intent_id("-1")
-            score = -1
+            # case5 : error
+            else:
+                logging.info("Case5 : cannot understand input at all")
+                #share_data.set_intent_id("-1")
+                score = -1
 
-        return share_data, score
+            return share_data, score
+        except Exception as e :
+            raise Exception ("error on scoring {0}".format(e))
