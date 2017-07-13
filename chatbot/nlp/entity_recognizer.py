8ef05a44917594e1af36297bd73b2f15cdf2574f
seungwookim <tmddno1@naver.com>
2017-07-13 14:09:51 +0900

    adjust chatbot config for accuracy

--- chatbot/nlp/entity_recognizer.py
+++ chatbot/nlp/entity_recognizer.py
@@ -18,7 +18,7 @@ class EntityRecognizer(ShareData):
     def parse(self, share_data):
         logging.info("■■■■■■■■■■ NER 분석 전 : " + str(share_data.get_morphed_data()))
         ner_data = self._get_ner_data(' '.join(share_data.get_morphed_data()))
-        share_data = self._match_ngram_dict(share_data , share_data.get_morphed_data(), ner_data)
+        share_data, ner_data = self._match_ngram_dict(share_data , share_data.get_morphed_data(), ner_data)
         self._get_convert_data(ner_data, share_data)
         logging.info("■■■■■■■■■■ NER 분석 결과 : " + str(ner_data))
         return share_data
@@ -42,13 +42,14 @@ class EntityRecognizer(ShareData):
         result = self.bilstmcrf_model.run(self.ner_model_id, {"input_data": input_sentence, "num": 0, "clean_ans": False})
         return result
 
-    def _match_ngram_dict(self, share_data, input_sentence, ner_data):
+    def _match_ngram_dict(self, share_data, input_sentence, ner_data_input):
         """
         match ngram dict with ner analized 
         :return: 
         """
         try :
             result = {}
+            ner_data = ner_data_input.copy()
             if (len(input_sentence) != len(ner_data)):
                 logging.info("■■■■■■■■■■ 길이 차이로 NER 처리 불가 ■■■■■■■■■■")
                 pass
@@ -56,6 +57,7 @@ class EntityRecognizer(ShareData):
             if(ChatKnowledgeMemDict.ngram.get(self.cb_id) == None):
                 cb_data = ChatKnowledgeMemDict.data.get(self.cb_id)
                 dist_keys = dict(Counter(ner_data))
+                index = 0
                 for key, val in zip(ner_data, input_sentence) :
                     if (key == 'O'):
                         continue
@@ -64,27 +66,37 @@ class EntityRecognizer(ShareData):
                         ner_conv = ' '.join(list(map(lambda x : x[0], list(filter(lambda x : x[1] == key, zip(input_sentence,ner_data))))))
                         result[key] = list(map(lambda x : x[0], model.search(ner_conv.replace(' ',''), threshold=1.0)))
                         if(len(result[key]) == 0) :
-                            result[key] = list(map(lambda x : x[0], model.search(ner_conv.replace(' ',''), threshold=0.2)))[0:4]
+                            result[key] = list(map(lambda x : x[0], model.search(ner_conv.replace(' ',''), threshold=0.3)))[0:4]
+                        # if (len(result[key]) == 0):
+                        #     result[key] = list(map(lambda x : x[0], model.search(ner_conv.replace(' ',''), threshold=0.1)))[0:8]
                         if(len(result[key]) == 0):
+                            logging.info("■■■■■■■■■■ NER 오류로 전수 조사 시작 (시간소요발생) ■■■■■■■■■■")
                             data, id = self.check_all_dict(ner_conv.replace(' ',''), cb_data)
-                            if(key != None) :
+                            if(id != None) :
                                 result[id] = data
                                 key = id
+                                ner_data_input[index] = id
                     else:
                         ner_conv = val
                         result[key] = list(map(lambda x: x[0], model.search(ner_conv, threshold=1.0)))
                         if (len(result[key]) == 0):
                             result[key] = list(map(lambda x : x[0], model.search(ner_conv, threshold=0.4)))[0:4]
+                        # if (len(result[key]) == 0):
+                        #     result[key] = list(map(lambda x : x[0], model.search(ner_conv.replace(' ',''), threshold=0.1)))[0:8]
                         if (len(result[key]) == 0):
+                            logging.info("■■■■■■■■■■ NER 오류로 전수 조사 시작 (시간소요발생) ■■■■■■■■■■")
                             data, id = self.check_all_dict(ner_conv, cb_data)
-                            if (key != None):
+                            if (id != None):
                                 result[id] = data
                                 key = id
+                                ner_data_input[index] = id
                     if(len(result[key]) == 0):
                         del result[key]
                     else :
-                        share_data.set_story_ner_entity(key, [ner_conv] + result[key])
-            return share_data
+                        share_data.set_story_ner_entity(key, result[key])
+
+                    index = index + 1
+            return share_data, ner_data_input
         except Exception as e :
             raise Exception ("Error on matching ngram afger bilstm crf : {0}".format(e))
 
