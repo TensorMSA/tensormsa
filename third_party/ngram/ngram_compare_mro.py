c07e84457d8c9521e28fbc50ac91f56481a6fad9
parksungchan <soul8085@gmail.com>
2017-12-06 10:01:54 +0900

    bug fix: ngram : encoding

--- third_party/ngram/ngram_compare_mro.py
+++ third_party/ngram/ngram_compare_mro.py
@@ -1,5 +1,6 @@
 import ngram
 import pandas as pd
+import logging
 
 class ThirdPartyNgram():
     def predict(self, node_id, param):
@@ -10,15 +11,18 @@ class ThirdPartyNgram():
         :return: 
         '''
         file_path = param['filepath']
-        df = pd.DataFrame.from_csv(file_path, sep='\t')
+        df = pd.DataFrame.from_csv(file_path, sep='\t', encoding = 'ISO-8859-1')
 
         for dfdata in df.values:
             param['list'].append({'item_code': dfdata[0], 'item_leaf': dfdata[1], 'item_desc': dfdata[2]})
 
         item = []
         for val in param['list']:
-            item_tuple = (val['item_code'].strip(), val['item_leaf'].strip(), val['item_desc'].strip())
-            item.append(item_tuple)
+            try:
+                item_tuple = (val['item_code'].strip(), val['item_leaf'].strip(), val['item_desc'].strip())
+                item.append(item_tuple)
+            except:
+                logging.info('Error Data'+val['item_code'])
 
         dataset = ngram.NGram(item, key=lambda x: x[2])
         dataset = sorted(dataset, key=lambda x: x[0])
