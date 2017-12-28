d9fa809d8f60e47cfefbb0fc8907980ed6ead8a9
parksungchan <soul8085@gmail.com>
2017-12-28 13:39:43 +0900

    upgrade: ngram : db connection

---
+++ third_party/ngram/db_conn.py
@@ -0,0 +1,24 @@
+import cx_Oracle
+import os
+
+class dbConnection():
+    def conn(self, param):
+        # os.putenv('NLS_LANG', 'UTF8')
+        # print(os.environ['LD_LIBRARY_PATH'])
+
+        conn = cx_Oracle.connect(param['conninfo'])
+        cur = conn.cursor()
+
+        sql = "select "
+        sql += "inventory_item_id, segment1, 'leaf_node' as leaf, description "
+        sql += "from inv.mtl_system_items_b "
+        sql += "where segment1 like 'Q43250%' "
+
+        cur.execute(sql)
+
+        for row in cur:
+            param['list'].append({'item_code': row[1], 'item_leaf': row[2], 'item_desc': row[3]})
+        cur.close()
+        conn = None
+        # conn.close()
+        return param['list']
