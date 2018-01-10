5e9bf2dc483a479f8b0d6ac26495e4abe4ff1602
parksungchan <soul8085@gmail.com>
2018-01-10 02:00:35 +0000

    update:image: eval log

--- demo/mro/api_mro_2_inceptionv4_train.py
+++ demo/mro/api_mro_2_inceptionv4_train.py
@@ -1,11 +1,12 @@
 import requests
 import json, os
 
-url = "{0}:{1}".format('0.0.0.0' , "8989")
-nn_id = "nn00000046"
-wf_ver_id = str(1)
+url = "{0}:{1}".format(os.environ['HOSTNAME'] , "8989")
+# url = '127.0.0.1:8989'
+nn_id = "nn00000019" # nn_id = "nn00000045"
+wf_ver_id = str(4)
 # ########################################################################################################################
-resp = requests.post('http://127.0.0.1:8989/api/v1/type/runmanager/state/train/nnid/'+nn_id+'/ver/'+wf_ver_id+'/')
+resp = requests.post('http://' + url + '/api/v1/type/runmanager/state/train/nnid/'+nn_id+'/ver/'+wf_ver_id+'/')
 data = json.loads(resp.json())
 print(data)
 for row in data:
@@ -15,86 +16,40 @@ for row in data:
     pred_name= rowdata['pred_name']
     pred_value= rowdata['pred_value']
 
-    print(true_name)
-    print(file_name)
-    print(pred_name)
-    print(pred_value)
+    # print(true_name)
+    # print(file_name)
+    # print(pred_name)
+    # print(pred_value)
     for rowsub in range(len(true_name)):
-        if true_name != pred_name[rowsub]:
-            print(true_name[rowsub]+'   ('+file_name[rowsub]+')')
-            print(pred_name[rowsub])
-            print(pred_value[rowsub])
-    print('end')
+        if true_name[rowsub] != pred_name[rowsub][0]:
+            print('---------------------------------------------------------------------------------------------------')
+            try:
+                index = pred_name[rowsub].index(true_name[rowsub])
+                if float(pred_value[rowsub][index]) < 20:
+                    print(true_name[rowsub] + '   (' + file_name[rowsub] + ')')
+                    print(pred_name[rowsub])
+                    print(pred_value[rowsub])
+            except:
+                print(true_name[rowsub] + '   (' + file_name[rowsub] + ')')
+                print(pred_name[rowsub])
+                print(pred_value[rowsub])
 
 
-    # print(print(true_name[i])+'('+file_name[i]+')')
-    # print(' - '+str(pred_name)+'('+str(pred_value)+')')
-# #
-# # def spaceprint(val, cnt):
-# #     leng = len(str(val))
-# #     cnt = cnt - leng
-# #     restr = ""
-# #     for i in range(cnt):
-# #         restr += " "
-# #     restr = restr+str(val)
-# #     return restr
-# #
-# # if data == None:
-# #     print(data)
-# # else:
-# #     try:
-# #         if data["status"] == "404":
-# #             print(data["result"])
-# #     except:
-# #         for train in data:
-# #             if train != None and train != "" and train != {} and train != "status" and train != "result":
-# #                 try:
-# #                     for tr in train["TrainResult"]:
-# #                         print(tr)
-# #                 except:
-# #                     maxcnt = 0
-# #                     line = ""
-# #                     for label in train["labels"]:
-# #                         if maxcnt<len(label)+2:
-# #                             maxcnt = len(label)+2
-# #
-# #                     for i in range(len(train["labels"])):
-# #                         for j in range(maxcnt+4):
-# #                             line += "="
-# #
-# #                     label_sub = []
-# #                     for label in train["labels"]:
-# #                         label = spaceprint(label,maxcnt)
-# #                         label_sub.append(label)
-# #
-# #                     space = ""
-# #                     for s in range(maxcnt):
-# #                         space +=" "
-# #
-# #                     print(space, label_sub)
-# #                     print(space, line)
-# #                     for i in range(len(train["labels"])):
-# #                         truecnt = 0
-# #                         totcnt = 0
-# #                         predict_sub = []
-# #                         for j in range(len(train["predicts"][i])):
-# #                             pred = spaceprint(train["predicts"][i][j],maxcnt)
-# #
-# #                             predict_sub.append(pred)
-# #                             totcnt += int(pred)
-# #                             # print(train["labels"].index(train["labels"][i]))
-# #                             if train["labels"].index(train["labels"][i]) == j:
-# #                                 truecnt = int(pred)
-# #                         if totcnt == 0:
-# #                             percent = 0
-# #                         else:
-# #                             percent = round(truecnt/totcnt*100,2)
-# #                         print(spaceprint(train["labels"][i],maxcnt), predict_sub, str(percent)+"%")
-#
-#
-# # println("E")
-#
-#
-#
+# true_name = ['airplain', 'bolt', 'airplain', 'airplain', 'ship']
+# file_name = ['images (49).jpg', 'images (14).jpg', 'images (24).jpg', 'images (27).jpg', 'images (11).jpg']
+# pred_name = [['airplain', 'car', 'bird'], ['airplain', 'car'], ['car', 'airplain'], ['car', 'airplain'], ['airplain', 'ship']]
+# pred_value = [['98.9', '0.89', '0.10'], ['100.', '3.44'], ['99.9', '0.04'], ['99.9', '15.00'], ['99.9', '23.00']]
 
+# for rowsub in range(len(true_name)):
+#     if true_name[rowsub] != pred_name[rowsub][0]:
+#         try:
+#             index = pred_name[rowsub].index(true_name[rowsub])
+#             if float(pred_value[rowsub][index]) < 100:
+#                 print(true_name[rowsub] + '   (' + file_name[rowsub] + ')')
+#                 print(pred_name[rowsub])
+#                 print(pred_value[rowsub])
+#         except:
+#             print(true_name[rowsub] + '   (' + file_name[rowsub] + ')')
+#             print(pred_name[rowsub])
+#             print(pred_value[rowsub])
 
